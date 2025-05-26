e539560cf69ec28371c4d9b82cbde7023a2cc2bb
heldersilva28 <103514875+heldersilva28@users.noreply.github.com>
2025-05-26 15:59:33 +0100

    add web

---
+++ backend/src/main/java/com/ipvc/bll/WebConfig.java
@@ -0,0 +1,18 @@
+package com.ipvc.bll;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://localhost:7216", "http://localhost:5161"
+                )
+                .allowedMethods("GET","POST","PUT","DELETE")
+                .allowedHeaders("*");
+    }
+}
+
---
+++ web/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using web;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp =>
+  new HttpClient { BaseAddress = new Uri("http://localhost/api/") });
+
+
+await builder.Build().RunAsync();
